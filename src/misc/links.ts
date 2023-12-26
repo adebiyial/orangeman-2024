e3f09a4f1ec16aa6bf4c03f527cec5cb37b21d68
Adebiyi Adedotun <adebiyiadedotun9@gmail.com>
2023-12-26 23:28:07 +0100

    Update site

---
+++ src/misc/links.ts
@@ -0,0 +1,16 @@
+export default {
+  websites: {
+    hatchPath: "https://hatchpath.io",
+    ceviant: "https://www.ceviant.co",
+    edgio: "https://edg.io",
+  },
+  pubBylines: {
+    "css-tricks": "https://css-tricks.com/author/adebiyial",
+    smashingmag: "https://www.smashingmagazine.com/author/adebiyi-adedotun",
+    logrocket: "https://blog.logrocket.com/author/adebiyial",
+  },
+  socials: {
+    twitter: "https://twitter.com/adebiyial",
+    mail: "mailto:adebiyiadedotun9@gmail.com",
+  },
+};
