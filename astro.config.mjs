75d8200818acd8f30c5248844669a803d7391703
Adebiyi Adedotun <adebiyiadedotun9@gmail.com>
2024-02-19 09:55:35 +0100

    Prepping for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
-import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
 import customCodeTheme from "./src/misc/code-theme.json";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "ignore",
   integrations: [mdx()],
   markdown: {
-    shikiConfig: { theme: customCodeTheme },
+    shikiConfig: {
+      theme: customCodeTheme,
+    },
   },
   output: "hybrid",
-  adapter: cloudflare(),
+  adapter: vercel(),
 });
